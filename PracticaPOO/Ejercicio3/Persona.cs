8d7139afed2a129c50063fc64871c02b0e9f160a
Byron Tercero <B1tercero.cruz@gmail.com>
2023-03-29 23:14:52 -0600

    Practis ejercice 3

---
+++ PracticaPOO/Ejercicio3/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPOO.Ejercicio3
+{
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+
+        public Persona(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public override string ToString()
+        {
+            return $"A caso te llamas como el pendejo de {Nombre}";
+        }
+
+        ~Persona() 
+        { 
+            Nombre = string.Empty ;
+        }
+    }
+}
