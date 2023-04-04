0ba30c02884ff915cd80063e031fd64ac6ddaf60
Byron Tercero <B1tercero.cruz@gmail.com>
2023-04-03 21:18:24 -0600

    Ejericice 7 Ordenador finish

---
+++ PracticaPOO/Ejercicio7/Ordenador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPOO.Ejercicio7
+{
+    internal class Ordenador
+    {
+        private int _disco;
+        private int _discoMax;
+        private bool _encendido;
+
+        public Ordenador(bool encendido)
+        {
+            _disco = 250;
+            _discoMax = 500;
+            _encendido = encendido;
+        }
+
+        public int AgregarDatos(int datos)
+        {
+            if (_encendido == true)
+            {
+                if (_disco >= _discoMax)
+                {
+                    _disco = _discoMax;
+                }
+
+                return _discoMax = datos;
+            }
+            else
+            {
+                return datos;
+            }
+        }
+
+
+
+
+    }
+}
