e3e2b1010113c29e7d0b2cbc5103af27dd1357a1
Byron Tercero <B1tercero.cruz@gmail.com>
2023-04-03 21:17:52 -0600

    Ejercice 5 finish

---
+++ PracticaPOO/Ejercicio5/Concesionario.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PracticaPOO.Ejercicio1;
+
+namespace PracticaPOO.Ejercicio5
+{
+    internal class Concesionario
+    {
+        private Coche[] _coches;
+        private int _limite;
+
+        private int _numCoche;
+
+        public Concesionario(int limite)
+        {
+            this._coches = new Coche[limite];
+            _limite = limite; // limite de coches que tendra el array
+            _numCoche = 0; // para recorrer el numero de coches que tendra en el array
+        }
+
+        public void agregarCoche(Coche c)
+        {
+            if (c != null && _numCoche < _coches.Length)
+            {
+                _coches[_numCoche] = c;
+                _numCoche++;
+            }
+        }
+        public void MostrarCoche()
+        {
+            for (int i = 0; i < _numCoche; i++)
+            {
+                Console.WriteLine(_coches[i].ToString());
+            }
+        }
+        public void VaciarCoche()
+        {
+            this._coches = new Coche[_limite];
+            _numCoche = 0;
+        }
+        public void EliminarCoche(Coche c)
+        {
+            if (c != null && _numCoche !=0)
+            {
+                int posicion = -1;
+                for (int i = 0; i < _numCoche; i++)
+                {
+                    if (c.ID == _coches[i].ID)
+                    {
+                        posicion = i;
+                    }
+                }
+                if (posicion == -1)
+                {
+                    Console.WriteLine("No existe el coche");
+                }
+                else
+                {
+                    _coches[posicion] = null;
+                    for (int i = posicion; i < _numCoche ; i++)
+                    {
+                        _coches[i] = _coches[i + 1];
+                    }
+
+                    _numCoche--;
+                }
+            }
+        }
+
+    }
+}
---
+++ PracticaPOO/Ejercicio5/Ejecucion5.cs
@@ -0,0 +1,38 @@
+﻿using PracticaPOO.Ejercicio1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPOO.Ejercicio5
+{
+    internal class Ejecucion5
+    {
+        void test()
+        {
+            Concesionario concesionario = new Concesionario(10);
+
+            Coche c1 = new Coche(1, "BMW", "4", 1000, 12000);
+            Coche c2 = new Coche(1, "Zentorno", "XS", 1000, 12000);
+            Coche c3 = new Coche(1, "Bugati", "4", 1000, 1222000);
+            Coche c4 = new Coche(1, "Hundai", "4", 1000, 125000);
+            Coche c5 = new Coche(1, "Toyota", "PRADO", 1000, 123000);
+
+
+            Console.WriteLine("Todos los coches");
+            concesionario.agregarCoche(c1);
+            concesionario.agregarCoche(c2);
+            concesionario.agregarCoche(c3);
+            concesionario.agregarCoche(c4);
+            concesionario.agregarCoche(c5);
+            concesionario.MostrarCoche();
+            Console.WriteLine("Eliminar coches los coches");
+            concesionario.EliminarCoche(c5);
+            concesionario.MostrarCoche();
+            Console.WriteLine("Vacio");
+            concesionario.VaciarCoche();
+            concesionario.MostrarCoche();
+        }
+    }
+}
