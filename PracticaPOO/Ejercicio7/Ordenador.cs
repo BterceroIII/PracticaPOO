5a743f08851c06d6f256edabdcd9111ec5b8ca83
Byron Tercero <B1tercero.cruz@gmail.com>
2023-04-05 20:06:11 -0600

    Practice Ejercicio7 Ordenador POO

--- PracticaPOO/Ejercicio7/Ordenador.cs
+++ PracticaPOO/Ejercicio7/Ordenador.cs
@@ -12,31 +12,86 @@ namespace PracticaPOO.Ejercicio7
         private int _discoMax;
         private bool _encendido;
 
-        public Ordenador(bool encendido)
+        public Ordenador()
         {
             _disco = 250;
             _discoMax = 500;
-            _encendido = encendido;
+            _encendido = false;
         }
 
         public int AgregarDatos(int datos)
         {
             if (_encendido == true)
             {
-                if (_disco >= _discoMax)
+
+            if (datos <= _discoMax)
+            {
+
+                _disco = datos + _disco;
+
+                if (_disco > _discoMax)
                 {
                     _disco = _discoMax;
                 }
 
-                return _discoMax = datos;
+                Console.WriteLine($"Se agrego al de disco {datos}");
+                Console.WriteLine($"Total disco de disco {_disco}");
+                return _disco;
             }
             else
             {
+                
+
+                return _disco = _discoMax;
+            }
+            }
+            else
+            {
+                Console.WriteLine($"El ordenador debe estar encendido");
                 return datos;
             }
         }
 
+        public int EliminarDatos(int datos)
+        {
+            if (_encendido == true)
+            {
 
+                if ( datos > 0)
+                {
+                    _disco = datos - _disco;
+
+                    Console.WriteLine($"Se elimino {datos}");
+                    return _disco;
+                }
+                else
+                {
+                     datos = 0;
+                    _disco = datos;
+
+                    Console.WriteLine($"Se elimino Todos los datos");
+                    return _disco;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"El ordenador debe estar encendido");
+                return datos;
+            }
+        }
+
+        public bool Encender()
+        {
+            Console.WriteLine("Ordenador encendido listo para usar");
+
+            return _encendido = true;
+        }
+
+        public bool Apagar()
+        {
+            Console.WriteLine("Ordenador apagado");
+            return _encendido = false;
+        }
 
 
     }
--- PracticaPOO/Program.cs
+++ PracticaPOO/Program.cs
@@ -1,6 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using PracticaPOO.Ejercicio1;
+using PracticaPOO.Ejercicio7;
 
+Ordenador ordenador = new Ordenador();
 
-
+ordenador.Encender();
+Console.WriteLine("------------------------");
+ordenador.AgregarDatos(500);
+ordenador.EliminarDatos(100);
+Console.WriteLine("------------------------");
+Console.WriteLine("------------------------");
+Console.WriteLine("------------------------");
+ordenador.Apagar();
+Console.WriteLine("------------------------");
+ordenador.AgregarDatos(100);
+ordenador.EliminarDatos(50);
 
